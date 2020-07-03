ea185cdbe893d6e7806d27aff601b6b46d0b3024
Perla SC <perlascabrera@gmail.com>
2020-07-03 13:11:02 -0500

    navbar styles, icon and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     purge: [],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'primary': '#f3c614',
+                'secundary': '#353535'
+            }
+        },
     },
     variants: {
         opacity: ['responsive', 'hover', 'focus', 'disabled'],
