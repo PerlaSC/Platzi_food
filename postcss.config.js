2a779ea52f6f0d18c0961b316190483b31f91e31
Perla SC <perlascabrera@gmail.com>
2020-07-06 14:49:27 -0500

    include pugecss and nanocss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
- module.exports = {
-     plugins: [
-         require('tailwindcss'),
-         require('autoprefixer')
-     ]
- }
+const purgecss = require('@fullhuman/postcss-purgecss')
+module.exports = {
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        purgecss({
+            content: ['./**/*.html']
+        }),
+        require('cssnano')({
+            preset: 'default',
+        }),
+    ]
+
+}
